d8142ea2fbc40a9a87868c5e2b4e837848773be4
MattBrown <matt.brown@avnet.com>
2023-10-26 16:24:33 -0600

    Initial commit

---
+++ win_install/upgrade_py_for_matlab_win10.bat
@@ -0,0 +1,10 @@
+ECHO Running script to update/upgrade python
+py -m pip install --user --upgrade pip
+py -m pip install pyserial
+py -m pip install numpy
+py -m pip install spectrum
+py -m pip install pandas
+py -m pip install openpyxl
+py -m pip install pyvisa
+ECHO Pausing for your review of install log...
+PAUSE
